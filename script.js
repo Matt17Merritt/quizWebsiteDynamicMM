ad6ea229977dff799bbb05b28b21b3a4bb8520a7
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-29 15:21:30 -0400

    stylized the website to prep for functions

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ var navEl = document.createElement("div");
 var listEls = document.createElement("ul");
 var highScoreLiEl = document.createElement("li");
 var timerLiEl = document.createElement("li");
-var h1El = document.createElement("h1");
-var paraEl = document.createElement("p");
+var mainDivEl = document.createElement("div");
+var h1ElCoding = document.createElement("h1");
+var paraElIntro = document.createElement("p");
 var startQuizBtn = document.createElement("button");
 
 // Stored li elements in a variable
 var listItems = document.getElementsByTagName("li");
 
 // Set the text content of my elements
-h1El.textContent = "Coding Quiz Challenge!";
+h1ElCoding.textContent = "Coding Quiz Challenge!";
 highScoreLiEl.textContent = "View Highscores";
 timerLiEl.textContent = "Time: ";
-paraEl.textContent =
+paraElIntro.textContent =
   "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
 startQuizBtn.textContent = "Start Quiz";
 
@@ -27,6 +28,19 @@ listEls.appendChild(highScoreLiEl);
 listEls.appendChild(timerLiEl);
 
 // Main content path
-body.appendChild(h1El);
-body.appendChild(paraEl);
-body.appendChild(startQuizBtn);
+body.appendChild(mainDivEl);
+mainDivEl.appendChild(h1ElCoding);
+h1ElCoding.appendChild(paraElIntro);
+h1ElCoding.appendChild(startQuizBtn);
+
+// Add classes to elements.
+navEl.className += "nav-class";
+listItems.className += "list-class"
+listEls.className += "ul-class";
+highScoreLiEl.className += "highscore-class";
+timerLiEl.className += "timer-class";
+mainDivEl.className += "mainDiv-class"
+h1ElCoding.className += "h1ElCoding-class";
+paraElIntro.className += "paraElIntro-class";
+startQuizBtn.className += "startQuizBtn-class"
+
