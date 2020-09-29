ab2b4cab126230ea9c27dbfb1c861548f6bd30d3
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-29 13:44:16 -0400

    dynamically created paraEl and start quiz button

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 // Elements that have been dynamically created
-
 var body = document.body;
 var navEl = document.createElement("div");
-var listEl = document.createElement("ul");
-var li1 = document.createElement("li");  
-var li2 = document.createElement("li");
-
+var listEls = document.createElement("ul");
+var highScoreLiEl = document.createElement("li");
+var timerLiEl = document.createElement("li");
 var h1El = document.createElement("h1");
-
-
-
+var paraEl = document.createElement("p");
+var startQuizBtn = document.createElement("button");
 
 // Stored li elements in a variable
-
 var listItems = document.getElementsByTagName("li");
 
 // Set the text content of my elements
-
 h1El.textContent = "Coding Quiz Challenge!";
-li1.textContent = "View Highscores";
-li2.textContent = "Time: "
+highScoreLiEl.textContent = "View Highscores";
+timerLiEl.textContent = "Time: ";
+paraEl.textContent =
+  "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+startQuizBtn.textContent = "Start Quiz";
 
 // Appended elements
-
+// Nav path
 body.appendChild(navEl);
-navEl.appendChild(listEl);
-listEl.appendChild(li1);
-listEl.appendChild(li2);
+navEl.appendChild(listEls);
+listEls.appendChild(highScoreLiEl);
+listEls.appendChild(timerLiEl);
 
+// Main content path
 body.appendChild(h1El);
+body.appendChild(paraEl);
+body.appendChild(startQuizBtn);
